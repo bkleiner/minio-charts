7f382532e7b84e8e10251ce75176032984e1880f
bkleiner <b.kleiner@str8labs.com>
2021-11-25 12:20:21 +0100

    reindex charts

--- reindex.sh
+++ reindex.sh
@@ -2,4 +2,4 @@
 
 helm package minio -d releases/
 
-helm repo index --merge index.yaml --url https://helm.min.io .
+helm repo index --merge index.yaml --url https://bkleiner.github.io/minio-charts/ .
